e0ef26eaa7dae7e2eb5f92d2944694e9bb6577a0
Alex Leonte <S305998@studenti.polito.it>
2022-11-10 21:46:36 +0100

    Added API route to retrieve all the hikes

--- code/server/controllers/HikeManager.js
+++ code/server/controllers/HikeManager.js
@@ -4,7 +4,35 @@ const Hike = require("../dao/model/Hike");
 const PersistentManager = require("../dao/PersistentManager");
 
 class HikeManager {
+  constructor() { }
 
+  async loadAllHikes() {
+    let hikes = await PersistentManager.loadAllRows(Hike.tableName);
+    if (hikes.length === 0) {
+      return Promise.reject({
+        code: 404,
+        result: "Hikes table is empty",
+      });
+    }
+
+    return Promise.resolve(hikes.map(hike => new Hike(
+      hike.hike_id,
+      hike.hut_id,
+      hike.writer_id,
+      hike.track_path,
+      hike.region,
+      hike.city,
+      hike.title,
+      hike.length,
+      hike.expected_time,
+      hike.ascent,
+      hike.difficulty,
+      hike.description,
+      hike.start_point,
+      hike.end_point,
+      hike.reference_point
+    )));
+  }
 }
 
 module.exports = new HikeManager();
--- code/server/routes/hike.router.js
+++ code/server/routes/hike.router.js
@@ -5,6 +5,17 @@ const { body, param, validationResult } = require("express-validator");
 const express = require('express');
 const router = express.Router();
 
-
+router.get('/', async (req, res) => {
+  try {
+    let hikes = await HikeManager.loadAllHikes();
+    console.log(hikes);
+    return res.status(200).json({ hikes });
+  } catch (exception) {
+    console.log(exception);
+    const errorCode = exception.code ?? 500;
+    const errorMessage = exception.result ?? 'Something went wrong, try again';
+    return res.status(errorCode).json({ error: errorMessage })
+  }
+})
 
 module.exports = router;
