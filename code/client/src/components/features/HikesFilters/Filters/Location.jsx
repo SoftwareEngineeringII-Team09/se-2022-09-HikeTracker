4a288cd2991c8107bcaf3b63ce0500bc1532b15b
Andrea Deluca <79919899+andrea-deluca@users.noreply.github.com>
2022-11-25 22:41:17 +0100

    Small bug fix for responsive in filters

--- code/client/src/components/features/HikesFilters/Filters/Location.jsx
+++ code/client/src/components/features/HikesFilters/Filters/Location.jsx
@@ -10,21 +10,21 @@ const Location = () => {
 
     return (
         <Row className="mb-5">
-            <Col xs={12} lg={4}>
+            <Col xs={12} lg={4} className="mb-3">
                 <Select id="location-region" name="geoArea.location.region" label="Region" defaultLabel="Leave this field empty">
                     {__REGIONS.map(r => (
                         <option key={r.regione} value={r.regione}>{r.nome}</option>
                     ))}
                 </Select>
             </Col>
-            <Col xs={12} lg={4}>
+            <Col xs={12} lg={4} className="mb-3">
                 <Select id="location-province" name="geoArea.location.province" label="Province" defaultLabel="Leave this field empty" disabled={!parseInt(region)}>
                     {getProvincesForRegion(parseInt(region)).map(p => (
                         <option key={p.provincia} value={p.provincia}>{p.nome}</option>
                     ))}
                 </Select>
             </Col>
-            <Col xs={12} lg={4}>
+            <Col xs={12} lg={4} className="mb-3">
                 <Select id="location-city" name="geoArea.location.city" label="City" defaultLabel="Leave this field empty" disabled={!parseInt(province)}>
                     {getCitiesForProvince(parseInt(province))
                         .map(c => (
