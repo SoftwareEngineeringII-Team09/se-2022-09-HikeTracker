7426d4771d26190b1ba05be306a33d5e043538f5
Alberto Baroso <albertobaroso999@gmail.com>
2022-11-27 21:03:03 +0100

    Login and signup E2E tests

--- code/client/src/services/config.js
+++ code/client/src/services/config.js
@@ -1,6 +1,9 @@
 const SERVER_PORT = process.env.NODE_ENV === "production" ? 80 : 3001
+const CLIENT_PORT = 3000
 const SERVER_URL = `http://localhost:${SERVER_PORT}/api`;
+const CLIENT_URL = `http://localhost:${CLIENT_PORT}/`;
 
 module.exports = {
-    SERVER_URL
+    SERVER_URL,
+    CLIENT_URL
 }
