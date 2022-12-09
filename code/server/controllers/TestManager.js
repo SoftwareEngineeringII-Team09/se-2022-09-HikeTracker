6bbb9aa1cd9b655f4f0627939dee0dc4c378a73a
Alberto Baroso <albertobaroso999@gmail.com>
2022-12-09 18:39:45 +0100

    Fake test to check coverage

---
+++ code/server/controllers/TestManager.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class TestClass {
+    testFunction() {
+        const x = 2;
+        console.log(x);
+    }
+}
+
+module.exports = new TestClass();
---
+++ code/server/test/__tests__/Test.test.js
@@ -0,0 +1,10 @@
+const TestManager = require('../../controllers/TestManager');
+
+describe("Test", function () {
+
+    test(`Should call test function`, function () {
+        TestManager.testFunction();
+    });
+
+
+});
