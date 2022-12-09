36dddcd35a1ea21fd9018d68ac0420a43610661a
Alex Leonte <S305998@studenti.polito.it>
2022-12-09 10:22:07 +0100

    Added capacity and altitude for Describe parking

--- code/client/src/services/parkingLots.js
+++ code/client/src/services/parkingLots.js
@@ -2,14 +2,9 @@ import { SERVER_URL } from "./config";
 import axios from 'axios';
 
 const parkingLots = {
-    addParkingLot: (parkingLotName, point) => {
+    addParkingLot: (data) => {
         return new Promise((resolve, reject) => {
-            axios.post(`${SERVER_URL}/parkingLots`, {
-                parkingLotName,
-                latitude: point.latitude,
-                longitude: point.longitude,
-                altitude: point.altitude,
-            }, { withCredentials: true })
+            axios.post(`${SERVER_URL}/parkingLots`, data, { withCredentials: true })
                 .then(res => resolve(res.data))
                 .catch(err => reject(err.response.data.error));
         })
