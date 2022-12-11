4a080f0fad21bcfec6a37f75f0db266369c4d1bb
Luca Scibetta s303976 <s303976@studenti.polito.it>
2022-12-11 19:11:12 +0100

    Fixed hut card frontend unit test

--- code/client/src/components/features/HutCard/HutCard.test.jsx
+++ code/client/src/components/features/HutCard/HutCard.test.jsx
@@ -25,7 +25,7 @@ const testHut = {
 }
 
 const expected = {
-    url: `/search/${testHut.hutId}`,
+    url: `/huts/${testHut.hutId}`,
 
     info: [
         { label: "hutName", value: /name/i },
@@ -39,21 +39,21 @@ const expected = {
 }
 
 describe("HutCard component", () => {
-    // it.each(expected.info)
-    //     ("Hut card $label is correctly rendered", (item) => {
-    //         render(<HutCard hut={testHut} />, { wrapper: MemoryRouter })
-    //         expect(screen.getByText(item.value)).toBeInTheDocument()
-    //     })
+    it.each(expected.info)
+        ("Hut card $label is correctly rendered", (item) => {
+            render(<HutCard hut={testHut} />, { wrapper: MemoryRouter })
+            expect(screen.getByText(item.value)).toBeInTheDocument()
+        })
 
     it("User is able to navigate to hut details when the card is clicked", async () => {
-        // const history = createMemoryHistory()
-        // render(
-        //     <Router location={history.location} navigator={history}>
-        //         <HutCard hut={testHut} />
-        //     </Router>
-        // )
-        // expect(screen.getByRole("link")).toHaveAttribute("href", expected.url)
-        // await userEvent.click(screen.getByRole("link"))
-        // expect(history.location.pathname).toBe(expected.url)
+        const history = createMemoryHistory()
+        render(
+            <Router location={history.location} navigator={history}>
+                <HutCard hut={testHut} />
+            </Router>
+        )
+        expect(screen.getByRole("link")).toHaveAttribute("href", expected.url)
+        await userEvent.click(screen.getByRole("link"))
+        expect(history.location.pathname).toBe(expected.url)
     })
 })
