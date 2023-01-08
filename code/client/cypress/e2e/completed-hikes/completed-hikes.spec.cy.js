3f4e3739259cdff612186bd0cb947eee8904c9cc
Andrea Deluca <79919899+andrea-deluca@users.noreply.github.com>
2023-01-08 21:16:04 +0100

    e2e fix

--- code/client/cypress/e2e/completed-hikes/completed-hikes.spec.cy.js
+++ code/client/cypress/e2e/completed-hikes/completed-hikes.spec.cy.js
@@ -1,14 +1,15 @@
 describe('List of completed hikes for an hiker', () => {
     before(() => {
         cy.clearAll()
-        cy.createUserWithDetails({ role: "Hiker" })
+        cy.createUserWithDetails({ role: "Local Guide" })
         cy.createHike({ writer: 1 })
+        cy.createHikerUser()
         cy.loginAsHiker()
-        cy.addCompletedHike({ selectedHike: 1, hike: 1, hiker: 1, status: 'finished', startTime: '01/01/2023, 01:01:01', endTime: '02/02/2023, 02:02:02'})
+        cy.addCompletedHike({ selectedHike: 1, hike: 1, hiker: 2, status: 'finished', startTime: '01/01/2023, 01:01:01', endTime: '02/02/2023, 02:02:02' })
     })
 
     beforeEach(() => cy.visit('/hikes/completed'))
-    //after(() => cy.clearAll())
+    after(() => cy.clearAll())
 
     it('should allows hiker to see the list of completed hikes', () => {
         cy.findAllByText(/Started/i).should('have.length', 1)
